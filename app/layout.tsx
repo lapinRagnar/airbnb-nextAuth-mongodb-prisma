c7c677178c5954b970080af4e6a7d9b3024d8623
lapinRagnar <lapinragnar@gmail.com>
2023-10-17 01:16:59 +0200

    creer les fichiers pour la Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/navbar/Navbar'
 
 const font = Nunito({ subsets: ['latin'] })
 
