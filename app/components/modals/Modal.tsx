5c88c3a14e4010f34074625e87ed8c98422abafb
lapinRagnar <lapinragnar@gmail.com>
2023-10-17 09:07:26 +0200

    on cree ModalProps pour typer le Modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+interface ModalProps {
+  isOpen: boolean
+  onClose: () => void
+  onSubmit: () => void
+  title?: string
+  body?: React.ReactElement
+  footer?: React.ReactElement
+  actionLabel: string
+  disabled?: boolean
+  secondaryAction?: () => void
+  secondaryLabel?: string
+}
 
-const Modal = () => {
+const Modal: React.FC<ModalProps> = () => {
   return (
     <div>Modal</div>
   )
