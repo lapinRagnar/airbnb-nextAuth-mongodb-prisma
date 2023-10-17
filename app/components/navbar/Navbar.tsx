7e1494b24147772f844f6e933e76869680d984cb
lapinRagnar <lapinragnar@gmail.com>
2023-10-17 02:55:34 +0200

    create toggle dropdown UserMenu

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+'use client'
+
 import Container from '../Container'
 import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
 
 const Navbar = () => {
+
+
+
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
       <div
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,18 @@
 
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
+import { useState, useCallback } from 'react'
+import MenuItem from './MenuItem'
 
 const UserMenu = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value)
+  }, [])
+
+
   return (
     <div className='relative'>
       <div
@@ -28,7 +38,7 @@ const UserMenu = () => {
         </div>
 
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className='
             p-4
             md:py-1
@@ -53,6 +63,38 @@ const UserMenu = () => {
         </div>
 
       </div>
+
+      { isOpen && (
+        <div
+          className='
+            absolute
+            rounded-xl
+            shadow-md
+            w-[40vw]
+            md:w-3/4
+            bg-white
+            overflow-hidden
+            right-0
+            top-12
+            text-sm
+          '
+        >
+          <div className='flex flex-col cursor-pointer'>
+            <>
+              <MenuItem 
+                onClick={() => {}}
+                label='Login'
+              />
+
+              <MenuItem 
+                onClick={() => {}}
+                label='Sign Up'
+              />
+            </>
+          </div>
+        </div>
+      )}
+
     </div>
   )
 }
