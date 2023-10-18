0bd6e957ca8f829538ce9956ab0228f0c46fcc24
lapinRagnar <lapinragnar@gmail.com>
2023-10-18 12:21:03 +0200

    quand on clique sur une category, on fait une query string sur l'url installation de query-string

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,4 +1,7 @@
+import { useRouter, useSearchParams } from "next/navigation"
+import { useCallback } from "react"
 import { IconType } from "react-icons"
+import qs from 'query-string'
 
 interface CategoryBoxPros {
   icon: IconType
@@ -8,8 +11,39 @@ interface CategoryBoxPros {
 }
 
 const CategoryBox: React.FC<CategoryBoxPros> = ({icon: Icon, label, selected, description}) => {
+
+  const router = useRouter()
+  const params = useSearchParams()
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {}
+
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label
+    } 
+
+    if (params?.get('category') === label) {
+      delete updatedQuery.category
+    }
+
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery
+    }, {skipNull: true})
+
+    router.push(url)
+
+  }, [label, params, router])
+
+
   return (
     <div
+      onClick={handleClick}
       className={`
         flex
         flex-col
